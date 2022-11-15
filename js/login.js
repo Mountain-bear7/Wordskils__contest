806026f6366a85bfc1e770aba2827adb563028f3
00XAH00 <pxaxulin@gmail.com>
2022-11-15 22:13:50 +0300

    remove unused code

--- js/login.js
+++ js/login.js
@@ -1,22 +1,3 @@
-//const form = {
-//    tel: document.getElementById('telephone'),
-//    email: document.getElementById('email'),
-//    password: document.getElementById('password'),
-//    button: document.querySelector('.button')
-//}
-//
-//function changesInput(e, name) {
-//    const { value } = e.target
-//    if (value) {
-//        form[name].classList.add('filled')
-//    } else {
-//        form[name].classList.remove('filled')
-//    }
-//}
-//
-//    form.tel.oninput = (e) => changesInput(e, 'tel')
-//    form.email.oninput = (e) => changesInput(e, 'email')
-//    form.password.oninput = (e) => changesInput(e, 'password')
 function user__login() {
     const user__phone = document.getElementById("user__phone").value;
     const user__password = document.getElementById("user__password").value;
