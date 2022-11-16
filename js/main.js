39683a10098750014266a78c176d592f7f990431
00XAH00 <pxaxulin@gmail.com>
2022-11-17 01:32:50 +0300

    add order

--- js/main.js
+++ js/main.js
@@ -32,9 +32,40 @@ slides.forEach((elem) => {
 })
 
 function viewDiv1(){
-    document.getElementById("customer-form2").style.display = "block";
+    const guest__blocks = document.getElementsByClassName("customer__data-wrapper");
+    const guest__rows = document.getElementById("guests__rows");
+    const guest__blocks__length = guest__blocks.length-1;
+    if (guest__blocks[guest__blocks__length].children.length !== 2) {
+        guest__blocks[guest__blocks__length].innerHTML +=
+            `<div class="customer__data">
+                <input type="text" name="customer__data-name" tabindex="1" placeholder="Имя">
+                <input type="text" name="customer__data-surname" tabindex="2" placeholder="Фамилия">
+                <input type="date" name="customer__data-birth" tabindex="3" placeholder="Дата рождения">
+                <input type="tel"  name="customer__data-num" tabindex="4" placeholder="Номер документа">
+            <!--<input type="number" name="customer__data-bonus" tabindex="6" placeholder="Количество баллов">-->
+                <input type="number" placeholder="Ряд" min="1" max="78">
+                <input type="number" placeholder="Место" min="1" max="100">
+            </div>`;
+    }
+    else {
+        guest__rows.innerHTML +=
+                `<div class="customer__data-wrapper">
+                    <div class="customer__data">
+                        <input type="text" name="customer__data-name" tabindex="1" placeholder="Имя">
+                        <input type="text" name="customer__data-surname" tabindex="2" placeholder="Фамилия">
+                        <input type="date" name="customer__data-birth" tabindex="3" placeholder="Дата рождения">
+                        <input type="tel"  name="customer__data-num" tabindex="4" placeholder="Номер документа">
+                        <!--<input type="number" name="customer__data-bonus" tabindex="6" placeholder="Количество баллов">-->
+                        <input type="number" placeholder="Ряд" min="1" max="78">
+                        <input type="number" placeholder="Место" min="1" max="100">
+                    </div>
+                </div>`;
+    }
+    user()
   };
 
   function viewDiv2(){
-    document.getElementById("customer-form2").style.display = "none";
+      const  guests = document.getElementsByClassName("customer__data");
+      if (guests.length-1 !== 0) guests[guests.length-1].remove();
+      user()
   };
